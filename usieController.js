db7c5ebc8de035432968127e16c8fb89209817fd
Amopho <amopho78@gmail.com>
2021-08-26 15:00:34 +0200

    adding controllers

---
+++ usieController.js
@@ -0,0 +1,7 @@
+const EmployeesData = require("../model/usiesModel");
+const express = require("express");
+// const {
+//   deleteMany,
+// } = require("../../20210824_rest_api_II/model/employeesModel");
+
+// A PUT request endpoint at /user/:name to update user from DB upon their name.
