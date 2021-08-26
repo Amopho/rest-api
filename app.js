969c731eea75c693f8797e357d31af11ac8fc84a
Amopho <amopho78@gmail.com>
2021-08-26 13:00:37 +0200

    root and usies endpoint created and working

--- app.js
+++ app.js
@@ -15,5 +15,12 @@ mongoose
   .catch((error) => {
     console.log(`There was a problem ${error.message}`);
   });
+// http://localhost:5000/usies
+const usies = require("./router/usies");
+app.use("/usies", usies);
+
+app.get("/", (req, res) => {
+  res.status(200).send("Welcome to our app");
+});
 
 module.exports = app;
